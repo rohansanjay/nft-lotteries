e6c4f23f6705c9db4f01c82062ee501d822dc9ce
Rohan Sanjay <rohanpsanjay@gmail.com>
2022-10-30 15:47:03 -0700

    script updates with vm

--- script/Deploy.sol
+++ script/Deploy.sol
@@ -5,23 +5,23 @@ import "forge-std/Script.sol";
 import { NFTLotteries } from "src/NFTLotteries.sol";
 
 contract Deploy is Script {
-    function run(
-        uint64 _subscriptionId, 
-        address _vrfCoordinator, 
-        bytes32 _keyHash,
-        uint256 _rake,
-        address _rakeRecipient
-    ) external {
-        vm.startBroadcast();
+    function run() external {
+        uint64 subscriptionId = uint64(vm.envUint("GOERLI_SUB_ID"));
+        address vrfCoordinator = vm.envAddress("GOERLI_VRF_COORDINATOR");
+        bytes32 keyHash = vm.envBytes32("GOERLI_KEY_HASH");
+        uint256 rake = vm.envUint("GOERLI_RAKE");
+        address rakeRecipient = vm.envAddress("GOERLI_RAKE_RECIPIENT");
 
+        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
 
+        vm.startBroadcast(deployerPrivateKey);
 
         NFTLotteries nftLotteries = new NFTLotteries(
-            _subscriptionId,
-            _vrfCoordinator,
-            _keyHash,
-            _rake,
-            _rakeRecipient
+            subscriptionId,
+            vrfCoordinator,
+            keyHash,
+            rake,
+            rakeRecipient
         );
 
         vm.stopBroadcast();
